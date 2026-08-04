2cd537c8a7100e4361fa172b3d08a98a1aace00a
artyomalvarez <alvarezmanjarrezjuanjose@gmail.com>
2026-08-04 18:41:06 -0500

    chore: remove unused test projects

--- VeterinaryClinicSystem.Test/UnitTest1.cs
+++ VeterinaryClinicSystem.Test/UnitTest1.cs
@@ -1,15 +1,9 @@
 ﻿namespace VeterinaryClinicSystem.Test;
 
-public class Tests
+public class UnitTest1
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
+    [Fact]
     public void Test1()
     {
-        Assert.Pass();
     }
 }
--- VeterinaryClinicSystem.Test2/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VeterinaryClinicSystem.Test2;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
