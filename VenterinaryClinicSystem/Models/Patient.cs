508dfa2cae954f084d9770b4ccd462299edfd1bc
artyomalvarez <alvarezmanjarrezjuanjose@gmail.com>
2026-08-11 10:53:00 -0500

    feat: methots and propiedids and made a constructor in de with one methond

---
+++ VenterinaryClinicSystem/Models/Patient.cs
@@ -0,0 +1,39 @@
+namespace VenterinaryClinicSystem.Models;
+
+public class Patient
+{
+    public Guid Id { get; set; }
+    public string Nombre { get; set; }
+    public byte Edad { get; set; }
+    public string Sintomas { get; set; }
+
+    public Patient(Guid id, string nombre, byte edad, string sintomas)
+    {
+        Id = id;
+        Nombre = nombre;
+        Edad = edad;
+        Sintomas = sintomas;
+    }
+
+    public void RegistrarPaciente(List<Patient> patients)
+    {
+
+    }
+
+    public void ListarPacientes(List<Patient> patients)
+    {
+        foreach (var patient in patients)
+        {
+            Console.WriteLine(patient.Nombre);
+            Console.WriteLine(patient.Edad);
+            Console.WriteLine(patient.Sintomas);
+        }
+    }
+    
+    public void BuscarPacientes(List<Patient> patients, string nombre)
+    {
+    }
+    
+ 
+    
+}
